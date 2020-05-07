10f14848fd1d4cb7bf69027a7dcd2bbbeae3b795
Sourabh Berde <sourabh.berde44@gmail.com>
2020-05-07 10:59:56 +0530

    Initial Commit for Streming project

---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+
+class StreamEdit extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id,formValues);
+    }
+
+    renderStream() {
+        if(this.props.stream) {
+            return (
+                <div>
+                    <h3>Edit a Stream</h3>
+                    <StreamForm
+                        initialValues={_.pick(this.props.stream, 'title', 'description')}
+                        onSubmit={this.onSubmit}
+                    />
+                </div>
+            )
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                {this.renderStream()}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    };
+}
+
+export default connect(mapStateToProps, {
+    fetchStream,
+    editStream
+})(StreamEdit);
